04aaa0cd062129e434b954de4cf5a0dbf4be04c2
Bachega <Gustavo.Bachega@thomsonreuters.com>
2025-03-05 15:13:13 -0300

    First commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule, RouterModule.forRoot(routes))
+  ]
+};
