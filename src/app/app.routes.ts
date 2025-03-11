8bd3384a09c7a9f11499c72879299b5cf26c5bee
Bachega <Gustavo.Bachega@thomsonreuters.com>
2025-03-11 20:15:06 -0300

    Adding alias to make imports more readable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { AccountComponent } from './components/account/account.component';
-import { LoginComponent } from './components/login/login.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { AccountComponent } from '@app/components/account/account.component';
+import { LoginComponent } from '@app/components/login/login.component';
+import { LandingComponent } from '@app/pages/landing/landing.component';
+import { MainLayoutComponent } from '@app/pages/main-layout/main-layout.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
