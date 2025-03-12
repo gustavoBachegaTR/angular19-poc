1a396866eb617d6bfecae9c35ec43f3c36c49fb2
Mejia <Daniel.MejiaBenjumea@thomsonreuters.com>
2025-03-12 14:56:08 -0500

    adds tertiary text to common header

--- src/app/shared/components/common-heading/common-heading.component.spec.ts
+++ src/app/shared/components/common-heading/common-heading.component.spec.ts
@@ -32,7 +32,7 @@ describe('CommonHeadingComponent', () => {
     expect(titleElement.textContent).toContain('Test Title');
   });
 
-  it('should display the subtitle', () => {
+  it('should display the subtitle if provided', () => {
     component.subtitle = 'Test Subtitle';
     fixture.detectChanges();
     const subtitleElement = fixture.debugElement.query(By.css('.common-heading__subtitle saf-text')).nativeElement;
@@ -40,9 +40,23 @@ describe('CommonHeadingComponent', () => {
   });
 
   it('should not display the subtitle if not provided', () => {
-    component.subtitle = '';
+    component.subtitle = undefined;
     fixture.detectChanges();
-    const subtitleElement = fixture.debugElement.query(By.css('.common-heading__subtitle saf-text')).nativeElement;
-    expect(subtitleElement.textContent).toBe('');
+    const subtitleElement = fixture.debugElement.query(By.css('.common-heading__subtitle saf-text'));
+    expect(subtitleElement).toBeNull();
+  });
+
+  it('should display the tertiary title if provided', () => {
+    component.tertiaryTitle = 'Test Tertiary Title';
+    fixture.detectChanges();
+    const tertiaryTitleElement = fixture.debugElement.query(By.css('.common-heading__tertiary saf-text')).nativeElement;
+    expect(tertiaryTitleElement.textContent).toContain('Test Tertiary Title');
+  });
+
+  it('should not display the tertiary title if not provided', () => {
+    component.tertiaryTitle = undefined;
+    fixture.detectChanges();
+    const tertiaryTitleElement = fixture.debugElement.query(By.css('.common-heading__tertiary saf-text'));
+    expect(tertiaryTitleElement).toBeNull();
   });
 });
--- src/app/shared/components/common-heading/common-heading.component.ts
+++ src/app/shared/components/common-heading/common-heading.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
 
 @Component({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-common-heading',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './common-heading.component.html',
   styleUrl: './common-heading.component.scss'
 })
 export class CommonHeadingComponent {
   @Input() title!: string;
   @Input() subtitle?: string;
+  @Input() tertiaryTitle?: string;
 }
